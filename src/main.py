ff984823384e8fd042af0c2b1af465ce43a5e378
tungbi811 <tungduynguyen811@gmail.com>
2025-12-24 10:49:57 +1100

    update main

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import mysql.connector
 from etl.extract import extract
 from etl.transform import transform
 from etl.load import load
+import time
 
 def run_etl(file_path: str, database):
     data = extract(file_path)
@@ -10,17 +11,9 @@ def run_etl(file_path: str, database):
     load(transformed_data, database)
 
 if __name__ == "__main__":
-    # conn = mysql.connector.connect(
-    #     host="localhost",
-    #     user="root",
-    #     password="Bi08112002#",
-    #     database="job_db"
-    # )
-
-    # cursor = conn.cursor()
-    # cursor.execute("Drop table job_db.jobs")
-
-    run_etl(
-        file_path="data/raw/jobs.csv",
-        database="job_db"
-    )
+    while (True):
+        run_etl(
+            file_path="data/raw/jobs.csv",
+            database="job_db"
+        )
+        time.sleep(86400) # Run automatically every 1 day
